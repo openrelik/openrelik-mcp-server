8fe31099e3b210112a8982752dc6ba55d99471a7
hacktobeer <ramsesdebeer@gmail.com>
2025-08-11 15:19:50 +0200

    Refactor docstrings

--- openrelik-mcp-server/openrelik_mcp_server/tools.py
+++ openrelik-mcp-server/openrelik_mcp_server/tools.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from .utils import get_openrelik_client
 
 from fastmcp import FastMCP
@@ -8,38 +10,53 @@ mcp = FastMCP(
 )
 
 
-@mcp.tool(
-    name="list_folder",
-    description="""
-    Lists files in an OpenRelik folder. Always returns a JSON string with the list of files with
+@mcp.tool()
+def list_folder(folder_id: int) -> list[dict[str, Any]]:
+    """Lists files in an OpenRelik folder. Always returns a JSON string with the list of files with
     their metadata.
-    """,
-)
-def list_folder(folder_id: int) -> str:
-    """Lists files in an OpenRelik folder."""
+
+    Args:
+        folder_id: The ID of the folder to list the files from.
+
+    Returns:
+        A list of dictionaries containing file metadata, including:
+        - display_name: The name of the file
+        - filesize: The size of the file in bytes
+        - magic_mime: The mime type of the file
+    """
     response = get_openrelik_client().get(f"/folders/{folder_id}/files/")
     return response.json()
 
 
-@mcp.tool(
-    name="read_file_metadata",
-    description="""
-    Reads a file metadata from a file in OpenRelik. Always returns a JSON string with the file metadata.
-    """,
-)
-def read_file_metadata(file_id: int) -> str:
-    """Reads a file metadata from a file in OpenRelik."""
+@mcp.tool()
+def read_file_metadata(file_id: int) -> dict[str, Any]:
+    """Reads a file metadata from a file in OpenRelik. Always returns a JSON string with the file metadata.
+
+    Args:
+        file_id: The ID of the file to get the metadata from.
+
+    Returns:
+        A dictionary containing file metadata, including:
+        - display_name: The name of the file
+        - filesize: The size of the file in bytes
+        - extension: The extension of the file
+        - original_path: The original path of the file where it was found on disk
+        - magic_mime: The mime type of the file
+        - hash_*: Several calculated unique forensic file hashes
+    """
     response = get_openrelik_client().get(f"/files/{file_id}")
     return response.json()
 
 
-@mcp.tool(
-    name="read_file_content",
-    description="""
-    Reads the content of a file in OpenRelik. Always returns a JSON string with the file content.
-    """,
-)
+@mcp.tool()
 def read_file_content(file_id: int) -> str:
-    """Reads a file content from a file in OpenRelik."""
+    """Reads the content of a file in OpenRelik. Always returns the file content.
+
+    Args:
+        file_id: The ID of the file to read the content from.
+
+    Returns:
+        The content of the file.
+    """
     response = get_openrelik_client().get(f"/files/{file_id}/content/")
-    return response.json()
+    return response.text
