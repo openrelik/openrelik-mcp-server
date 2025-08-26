c9790a2f2fd4e3cd2572aa5c189fd67debb63260
hacktobeer <ramsesdebeer@gmail.com>
2025-08-26 08:51:47 +0200

    refactor read_file_content

--- openrelik-mcp-server/openrelik_mcp_server/tools.py
+++ openrelik-mcp-server/openrelik_mcp_server/tools.py
@@ -1,3 +1,4 @@
+import base64
 from typing import Any
 
 from .utils import get_openrelik_client
@@ -58,5 +59,5 @@ def read_file_content(file_id: int) -> str:
     Returns:
         The content of the file.
     """
-    response = get_openrelik_client().get(f"/files/{file_id}/content/")
-    return response.text
+    response = get_openrelik_client().get(f"/files/{file_id}/download")
+    return base64.b64decode(response.content)
